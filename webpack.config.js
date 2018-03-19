580472240b9c557adeaa0d32f30d54bd99fd6c0b
Ryan Johnson <ryj1023@yahoo.com>
2018-03-18 20:33:13 -0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@
 switch (process.env.NODE_ENV) {
   case 'prod':
   case 'production':
-    module.exports = require('./config/webpack.prod');
+    module.exports = require('./configuration/webpack.prod');
     break;
 
   case 'dev':
   case 'development':
   default:
-    module.exports = require('./config/webpack.dev');
+    module.exports = require('./configuration/webpack.dev');
 }
  // we need both client and server side configuration
 
