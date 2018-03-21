979dc438170e8d83a04c968db88d7ef8247a10c4
Ryan Johnson <ryj1023@yahoo.com>
2018-03-20 20:31:05 -0400

    update

--- configuration/config.js
+++ configuration/config.js
@@ -1,6 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'yarn mongodb://heroku_mrtn77bc:5d12vccr3nmqm36qavj33109lm@ds229918.mlab.com:29918/heroku_mrtn77',
+  db: 'mongodb://heroku_mrtn77bc:5d12vccr3nmqm36qavj33109lm@ds229918.mlab.com:29918/heroku_mrtn77',
   db_dev: 'mongodb://localhost/users',
 };
