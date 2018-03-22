fb034d8dd0a172eff0c6022fc9a59d3a47db32e1
Ryan Johnson <ryj1023@yahoo.com>
2018-03-21 22:19:15 -0400

    update

--- configuration/config.js
+++ configuration/config.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   // db: 'mongodb://heroku_mrtn77bc:5d12vccr3nmqm36qavj33109lm@ds229918.mlab.com:29918/heroku_mrtn77',
+  // db: 'mongodb://ryanj:Focus0816@ds221609.mlab.com:21609/fa_user_data',
   db: process.env.MONGOLAB_URI,
   db_dev: 'mongodb://localhost/users',
 };
