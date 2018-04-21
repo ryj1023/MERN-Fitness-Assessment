b7e5f4bc361354906b1ee557cff2594443fb084f
Ryan Johnson <ryj1023@yahoo.com>
2018-04-21 13:03:13 -0400

    menu dropdown and repsonsive css

--- client/app/components/navigations/navigation.js
+++ client/app/components/navigations/navigation.js
@@ -31,12 +31,22 @@ export default class Navigation extends Component{
                 <div>
                     <nav className='nav-main'>
                         <div> <Link to='/' className="logo">Lets Get Fit </Link></div>
-                            <ul>
-                            <li><Link to='./profile' className='nav-item'>Profile</Link></li>
-                            <li><Link to='./assessment' className='nav-item'>Fitness Assessment</Link></li>
-                            <li><Link to='./food-search' href="#" className='nav-item'>Food Search</Link></li>
-                            </ul>
+                            <Link to='./profile' className='nav-item'>Profile</Link>
+                            <Link to='./assessment' className='nav-item'>Fitness Assessment</Link>
+                            <Link to='./food-search' className='nav-item'>Food Search</Link>
                             <Link to='/' onClick={(e) => this.logout(e)}className='login'>Logout</Link>
+                                <ul className='dropdown-ul'>
+                                    <li><a href="#" className='nav-item dropdown-menu'>Menu</a>
+                                        <div className="nav-sub">
+                                            <ul>
+                                                <Link className='dropdown-link' to='./profile'>Profile</Link>
+                                                <Link className='dropdown-link' to='./assessment'>Fitness Assessment</Link>
+                                                <Link className='dropdown-link' to='./food-search'>Food Search</Link>
+                                                <Link className='dropdown-link' to='/' onClick={(e) => this.logout(e)}>Logout</Link>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </ul>
                     </nav>
                 </div>
             ) 
@@ -45,12 +55,20 @@ export default class Navigation extends Component{
             <div>
                 <nav className='nav-main'>
                     <div> <Link to='/' className="logo">Lets Get Fit </Link></div>
-                        <ul>
-                        <li><Link to='./assessment' href="#" className='nav-item'>Fitness Assessment</Link></li>
-                        <li><Link to='./food-search' href="#" className='nav-item'>Food Search</Link></li>
-                        </ul>
                         <Link to='./login' href="./login" className='login'>Login</Link>
                         <Link to='./sign-up' href="./sign-up" className='login'>Sign Up</Link>
+                        <ul className='dropdown-ul'>
+                            <li><a href="#" className='nav-item dropdown-menu'>Menu</a>
+                                <div className="nav-sub">
+                                    <ul>
+                                        <Link className='dropdown-link' to='./assessment'>Fitness Assessment</Link>
+                                        <Link className='dropdown-link' to='./food-search'>Food Search</Link>
+                                        <Link className='dropdown-link' to='./login' href="./login">Login</Link>
+                                        <Link className='dropdown-link' to='./sign-up' href="./sign-up">Sign Up</Link> 
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
                 </nav>
             </div>
         ) 
