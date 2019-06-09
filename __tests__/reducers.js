ccc03970932a653216429aa75b57c515bded67b1
Ryan Johnson <ryj1023@yahoo.com>
2019-06-09 17:52:59 -0400

    added mocked async calls

--- __tests__/reducers.js
+++ __tests__/reducers.js
@@ -1,14 +1,39 @@
 import * as actions from '../client/app/actions'
+import axios from 'axios'
+jest.setTimeout(10000)
 
-describe('actions', () => {
-    it('should create an action to add a todo', () => {
-        const text = 'Finish docs'
-        const expectedAction = {
-            type: types.ADD_TODO,
-            text,
-        }
-        expect(actions.addTodo(text)).toEqual(expectedAction)
-    })
+const getPeoplePromise = async axios => {
+    const { data } = await axios.get('https://swapi.co/api/people')
+    return {
+        count: data.count,
+        results: data.results,
+    }
+}
+
+// it('should return data from an API', async done => {
+//     expect.assertions(2)
+//     const data = await getPeoplePromise(axios)
+//     expect(data.count).toEqual(87)
+//     expect(data.results.length).toBeGreaterThan(2)
+//     done()
+// })
+
+it('get people returns count and results', async done => {
+    const mockFetch = {}
+    mockFetch.get = jest.fn().mockReturnValue(
+        Promise.resolve({
+            data: {
+                count: 87,
+                results: [1, 2, 3, 4, 5],
+            },
+        })
+    )
+
+    expect.assertions(2)
+    const data = await getPeoplePromise(mockFetch)
+    expect(mockFetch.get.mock.calls.length).toBe(1)
+    expect(data.results.length).toBeGreaterThan(2)
+    done()
 })
 
 /*
