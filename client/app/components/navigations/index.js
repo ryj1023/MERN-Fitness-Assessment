38b7a50bc426871c941fcacc406648a9f305a292
Ryan Johnson <ryj1023@yahoo.com>
2020-05-30 17:54:58 -0400

    Update

--- client/app/components/navigations/index.js
+++ client/app/components/navigations/index.js
@@ -1,5 +1,5 @@
-import React, { Component, useState, useEffect } from './node_modules/react'
-import Drawer from './node_modules/react-motion-drawer'
+import React, { Component } from 'react'
+import Drawer from 'react-motion-drawer'
 import {
     Button,
     UncontrolledButtonDropdown,
@@ -8,126 +8,133 @@ import {
     DropdownToggle,
     ListGroup,
     ListGroupItem,
-} from './node_modules/reactstrap'
-import Link from './node_modules/next/link'
-import { AlignJustify } from './node_modules/react-feather'
+} from 'reactstrap'
+import Link from 'next/link'
+import { AlignJustify } from 'react-feather'
 
-const Navigation = ({ loggedIn, loading }) => {
-    const [openRight, setOpenRight] = useState(false)
+export default class Navigation extends Component {
+    state = {
+        loggedIn: false,
+        noTouchOpen: false,
+        noTouchClose: false,
+        openRight: false,
+        isLoading: true,
+    }
 
-    const logout = e => {
+    logout(e) {
         e.preventDefault()
         localStorage.clear('user')
         window.location = '/'
     }
 
-    useEffect(() => {
-        setOpenRight(false)
-    }, [])
-    if (loading) return null
-    return (
-        <div>
-            <Drawer
-                fadeOut
-                open={openRight}
-                onChange={open => {
-                    setOpenRight(open)
-                }}
-                drawerStyle={{ backgroundColor: 'white' }}
-            >
-                <div className="w-100 d-flex flex-column side-rail-list">
-                    <Link href={{ pathname: '/' }}>
-                        <a
-                            id="dashboard"
-                            onClick={() => this.setState({ openRight: false })}
-                            className="w-100 rounded-0 text-dark font-weight-bold text-decoration-none p-3"
-                        >
-                            Dashboard
-                        </a>
-                    </Link>
-                    <Link href={{ pathname: '/assessment' }}>
-                        <a
-                            onClick={() => this.setState({ openRight: false })}
-                            className="w-100 rounded-0 text-dark font-weight-bold text-decoration-none p-3 border-top"
-                        >
-                            Assessment
-                        </a>
-                    </Link>
-                    {/* <Link href={{ pathname: '/food-search' }}>
+    componentDidMount() {
+        this.setState({ openRight: false })
+    }
+    render() {
+        const { openRight, noTouchOpen, noTouchClose, isLoading } = this.state
+        const { loggedIn, loading } = this.props
+        if (loading) return null
+        return (
+            <div>
+                <Drawer
+                    fadeOut
+                    open={openRight}
+                    onChange={open => {
+                        this.setState({ openRight: open })
+                    }}
+                    drawerStyle={{ backgroundColor: 'white' }}
+                >
+                    <div className="w-100 d-flex flex-column side-rail-list">
+                        <Link href={{ pathname: '/' }}>
+                            <a
+                                id="dashboard"
+                                onClick={() =>
+                                    this.setState({ openRight: false })
+                                }
+                                className="w-100 rounded-0 text-dark font-weight-bold text-decoration-none p-3"
+                            >
+                                Dashboard
+                            </a>
+                        </Link>
+                        <Link href={{ pathname: '/fitness-assessment' }}>
                             <a
                                 onClick={() =>
                                     this.setState({ openRight: false })
                                 }
                                 className="w-100 rounded-0 text-dark font-weight-bold text-decoration-none p-3 border-top"
                             >
-                                Add Foods
+                                Fitness Assessment
                             </a>
-                        </Link> */}
-                    <Link href={{ pathname: '/my-goals' }}>
-                        <a
-                            onClick={() => this.setState({ openRight: false })}
-                            className="w-100 rounded-0 text-dark font-weight-bold text-decoration-none p-3 border-top"
-                        >
-                            My Goals
-                        </a>
-                    </Link>
-                    {loggedIn ? (
-                        <div
-                            onClick={e => this.logout(e)}
-                            className="text-left w-100 rounded-0 text-dark font-weight-bold p-3 border-top border-bottom"
-                        >
-                            Log Out
-                        </div>
-                    ) : (
-                        <Link href={{ pathname: '/login' }}>
+                        </Link>
+                        <Link href={{ pathname: '/food-search' }}>
                             <a
                                 onClick={() =>
                                     this.setState({ openRight: false })
                                 }
-                                className="w-100 rounded-0 text-dark font-weight-bold text-decoration-none p-3 border-top border-bottom"
+                                className="w-100 rounded-0 text-dark font-weight-bold text-decoration-none p-3 border-top"
                             >
-                                Log In
+                                Food Search
                             </a>
                         </Link>
-                    )}
-                    {!loggedIn && (
-                        <Link href={{ pathname: '/sign-up' }}>
+                        <Link href={{ pathname: '/my-nutrition' }}>
                             <a
                                 onClick={() =>
                                     this.setState({ openRight: false })
                                 }
-                                className="text-left w-100 rounded-0 text-dark text-decoration-none font-weight-bold p-3 border-top border-bottom"
+                                className="w-100 rounded-0 text-dark font-weight-bold text-decoration-none p-3 border-top"
                             >
-                                Sign Up
+                                My Nutrition
                             </a>
                         </Link>
-                    )}
-                </div>
-            </Drawer>
-            <nav className="nav-main">
-                <div className="d-flex align-items-center">
-                    <Link href={{ pathname: '/' }}>
-                        <a className="logo btn btn-lg rounded-0 text-white nav-item">
-                            Let's Get Fit
-                        </a>
-                    </Link>
-                    <Link href={{ pathname: '/assessment' }}>
-                        <a
-                            style={{ whiteSpace: 'nowrap' }}
-                            className="d-none d-sm-flex nav-item btn btn-lg rounded-0 text-white"
-                        >
-                            Assessment
-                        </a>
-                    </Link>
-                    <Link href={{ pathname: '/my-goals' }}>
-                        <a
-                            style={{ whiteSpace: 'nowrap' }}
-                            className="d-none d-sm-flex nav-item btn btn-lg rounded-0 text-white"
-                        >
-                            My Goals
-                        </a>
-                    </Link>
-                    {/* <UncontrolledButtonDropdown className="dropdown-container d-none d-sm-flex">
+                        {loggedIn ? (
+                            <div
+                                onClick={e => this.logout(e)}
+                                className="text-left w-100 rounded-0 text-dark font-weight-bold p-3 border-top border-bottom"
+                            >
+                                Log Out
+                            </div>
+                        ) : (
+                            <Link href={{ pathname: '/login' }}>
+                                <a
+                                    onClick={() =>
+                                        this.setState({ openRight: false })
+                                    }
+                                    className="w-100 rounded-0 text-dark font-weight-bold text-decoration-none p-3 border-top border-bottom"
+                                >
+                                    Log In
+                                </a>
+                            </Link>
+                        )}
+                        {!loggedIn && (
+                            <Link href={{ pathname: '/sign-up' }}>
+                                <a
+                                    onClick={() =>
+                                        this.setState({ openRight: false })
+                                    }
+                                    className="text-left w-100 rounded-0 text-dark text-decoration-none font-weight-bold p-3 border-top border-bottom"
+                                >
+                                    Sign Up
+                                </a>
+                            </Link>
+                        )}
+                    </div>
+                </Drawer>
+                <nav className="nav-main">
+                    <div className="d-flex align-items-center">
+                        <Link href={{ pathname: '/' }}>
+                            <a className="logo btn btn-lg rounded-0 text-white nav-item">
+                                Let's Get Fit
+                            </a>
+                        </Link>
+                        <Link href={{ pathname: '/fitness-assessment' }}>
+                            <a
+                                style={{ whiteSpace: 'nowrap' }}
+                                className="d-none d-sm-flex nav-item btn btn-lg rounded-0 text-white"
+                            >
+                                Fitness Assessment
+                            </a>
+                        </Link>
+                        <UncontrolledButtonDropdown className="dropdown-container d-none d-sm-flex">
                             <DropdownToggle
                                 className="dropdown-button btn-lg border-0 rounded-0"
                                 caret
@@ -138,99 +145,99 @@ const Navigation = ({ loggedIn, loading }) => {
                                 <DropdownItem className="p-0">
                                     <Link href={{ pathname: '/my-nutrition' }}>
                                         <a className="text-white btn w-100 btn-lg rounded-0">
-                                            My Goals
+                                            My Nutrition
                                         </a>
                                     </Link>
                                 </DropdownItem>
                                 <DropdownItem className="p-0">
                                     <Link href={{ pathname: '/food-search' }}>
                                         <a className="text-white btn w-100 btn-lg rounded-0">
-                                            Add Foods
+                                            Food Search
                                         </a>
                                     </Link>
                                 </DropdownItem>
                             </DropdownMenu>
-                        </UncontrolledButtonDropdown> */}
-                    {loggedIn ? (
-                        <Button
-                            onClick={e => this.logout(e)}
-                            className="d-none d-sm-flex login ml-auto btn-lg border-0 rounded-0"
-                        >
-                            Logout
-                        </Button>
-                    ) : (
-                        <Link href={{ pathname: '/login' }} href="./login">
-                            <a className="text-white btn btn-lg rounded-0 nav-item d-sm-flex d-none ml-auto">
-                                Login
-                            </a>
-                        </Link>
-                    )}
-                    {!loggedIn && (
-                        <Link href={{ pathname: '/sign-up' }}>
-                            <a
-                                style={{ whiteSpace: 'nowrap' }}
-                                className="d-none d-sm-flex nav-item btn btn-lg rounded-0 text-white"
+                        </UncontrolledButtonDropdown>
+                        {loggedIn ? (
+                            <Button
+                                onClick={e => this.logout(e)}
+                                className="d-none d-sm-flex login ml-auto btn-lg border-0 rounded-0"
                             >
-                                Sign Up
+                                Logout
+                            </Button>
+                        ) : (
+                            <Link href={{ pathname: '/login' }} href="./login">
+                                <a className="text-white btn btn-lg rounded-0 nav-item d-sm-flex d-none ml-auto">
+                                    Login
+                                </a>
+                            </Link>
+                        )}
+                        {!loggedIn && (
+                            <Link href={{ pathname: '/sign-up' }}>
+                                <a
+                                    style={{ whiteSpace: 'nowrap' }}
+                                    className="d-none d-sm-flex nav-item btn btn-lg rounded-0 text-white"
+                                >
+                                    Sign Up
+                                </a>
+                            </Link>
+                        )}
+                        <div className="w-100 d-flex justify-content-end">
+                            <a
+                                className="d-sm-none hamburger-menu btn btn-lg rounded-0"
+                                onClick={() =>
+                                    this.setState({ openRight: !openRight })
+                                }
+                            >
+                                <AlignJustify color="white" />
                             </a>
-                        </Link>
-                    )}
-                    <div className="w-100 d-flex justify-content-end">
-                        <a
-                            className="d-sm-none hamburger-menu btn btn-lg rounded-0"
-                            onClick={() =>
-                                this.setState({ openRight: !openRight })
-                            }
-                        >
-                            <AlignJustify color="white" />
-                        </a>
+                        </div>
                     </div>
-                </div>
-            </nav>
-            <style jsx>{`
-                .side-rail-list a:hover,
-                .side-rail-list div:hover {
-                    background: #f5f5f5;
-                    cursor: pointer;
-                }
-                .hamburger-menu {
-                    cursor: pointer;
-                }
-                .logo {
-                    white-space: nowrap;
-                }
-                nav,
-                nav :global(.list-group-item, .btn-lg) {
-                    background-color: #454545;
-                }
-                div :global(.dropdown-item) {
-                    white-space: normal;
-                }
-                .dropdown-list {
-                    height: 70px;
-                    padding: 1.1em;
-                    background-color: #454545;
-                    cursor: pointer;
-                    color: white;
-                    text-decoration: none;
-                    text-align: center;
-                }
-                .dropdown-list:hover,
-                .hamburger-menu:hover,
-                .nav-item:hover,
-                div :global(.dropdown-item a:hover) {
-                    background-color: #5a6268 !important;
-                    text-decoration: none;
-                    color: white;
-                }
-                .icon {
-                    width: 45%;
-                    height: 5px;
-                    background-color: white;
-                    margin: 6px auto;
-                }
-            `}</style>
-        </div>
-    )
+                </nav>
+                <style jsx>{`
+                    .side-rail-list a:hover,
+                    .side-rail-list div:hover {
+                        background: #f5f5f5;
+                        cursor: pointer;
+                    }
+                    .hamburger-menu {
+                        cursor: pointer;
+                    }
+                    .logo {
+                        white-space: nowrap;
+                    }
+                    nav,
+                    nav :global(.list-group-item, .btn-lg) {
+                        background-color: #454545;
+                    }
+                    div :global(.dropdown-item) {
+                        white-space: normal;
+                    }
+                    .dropdown-list {
+                        height: 70px;
+                        padding: 1.1em;
+                        background-color: #454545;
+                        cursor: pointer;
+                        color: white;
+                        text-decoration: none;
+                        text-align: center;
+                    }
+                    .dropdown-list:hover,
+                    .hamburger-menu:hover,
+                    .nav-item:hover,
+                    div :global(.dropdown-item a:hover) {
+                        background-color: #5a6268 !important;
+                        text-decoration: none;
+                        color: white;
+                    }
+                    .icon {
+                        width: 45%;
+                        height: 5px;
+                        background-color: white;
+                        margin: 6px auto;
+                    }
+                `}</style>
+            </div>
+        )
+    }
 }
-export default Navigation
