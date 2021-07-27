6d3d814ce404eba631b918f404cb88fc06de4dea
Ryan Johnson <ryj1023@yahoo.com>
2021-07-27 12:52:41 -0400

    Removed env

--- client/app/reducers/question-display.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-
-export default class QuestionDisplay extends Component{
-	render(){
-		return <li>{this.props.question}</li>
-	}
-}
-
--- next.config.js
+++
@@ -1,4 +0,0 @@
-const withCSS = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
-const withOffline = require('next-offline')
-module.exports = withCSS(withSass(withOffline({ generateInDevMode: true })))
