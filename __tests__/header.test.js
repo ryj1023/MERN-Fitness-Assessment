d60b4c46243e709418921071bf0250bb32c26e87
Ryan Johnson <ryj1023@yahoo.com>
2021-07-27 12:51:29 -0400

    removed env

--- __tests__/header.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Header from '../client/app/components/headers'
-import { shallow } from 'enzyme'
-
-it('expect to render header component', () => {
-    expect(shallow(<Header />)).toMatchSnapshot()
-})
--- client/app/reducers/get-food-recipes.js
+++
@@ -1,8 +0,0 @@
-const getFoodRecipes = (state = { recipes: [] }, action) => {
-    if (action.type === 'RECIPES') {
-        return { ...state, recipes: action.payload }
-    }
-    return state
-}
-
-export default getFoodRecipes
